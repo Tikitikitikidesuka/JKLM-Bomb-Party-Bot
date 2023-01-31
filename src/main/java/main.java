ed09952bb78e2d479d2c18e6fb2d4e5b91e293e7
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-31 17:27:10 +0100

    Add htmlunit library test code to main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,18 @@
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 public class main {
+    public static void main(String[] args) {
+        try (final WebClient webClient = new WebClient()) {
+            final HtmlPage page = webClient.getPage("https://htmlunit.sourceforge.io/");
+            System.out.println(page.getTitleText());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
