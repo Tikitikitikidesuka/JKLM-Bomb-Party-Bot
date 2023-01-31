f52a9c22c551e9b6e42e79fe3bc34287b726933b
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-31 20:57:52 +0100

    Add attempt to press JoinGame button

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,7 @@
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -15,15 +13,29 @@ public class main {
             webClient.getOptions().setFetchPolyfillEnabled(true);
             webClient.getOptions().setJavaScriptEnabled(true);
             webClient.getOptions().setCssEnabled(false);
-            final HtmlPage page = webClient.getPage("https://jklm.fun/GYGE");
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            HtmlPage page = webClient.getPage("https://jklm.fun/NXSV");
             System.out.println(page.getTitleText());
 
             HtmlInput namePrompt = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/input");
-            namePrompt.setValueAttribute("elGoog");
+            namePrompt.setValueAttribute("elGoog32");
             HtmlButton submitNameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/button");
             submitNameButton.click();
-            webClient.waitForBackgroundJavaScript(50 * 500);
+            webClient.waitForBackgroundJavaScript(5000);
             System.out.println(page.getWebResponse().getContentAsString());
+
+
+
+
+            HtmlButton joinGameButton = null;
+            while(joinGameButton == null) {
+                System.out.println("Loading...");
+                page = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
+                webClient.waitForBackgroundJavaScript(5000);
+                System.out.println(page.getWebResponse().getContentAsString());
+                joinGameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/div[1]/div[1]/button");
+            }
+            joinGameButton.click();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
