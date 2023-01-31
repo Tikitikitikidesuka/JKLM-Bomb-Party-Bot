fd8a3ca3452d429a320957ed23329118d591efb0
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-31 23:44:44 +0100

    Add attempt to press JoinGame button with Selenium

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,7 @@
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLIFrameElement;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.io.IOException;
@@ -9,14 +10,51 @@ import java.util.List;
 
 public class main {
     public static void main(String[] args) {
-        try (final WebClient webClient = new WebClient()) {
+        try (final WebClient webClient = new WebClient(BrowserVersion.FIREFOX)) {
             webClient.getOptions().setFetchPolyfillEnabled(true);
             webClient.getOptions().setJavaScriptEnabled(true);
-            webClient.getOptions().setCssEnabled(false);
+            webClient.getOptions().setCssEnabled(true);
+            webClient.getOptions().setRedirectEnabled(true);
+            webClient.getCookieManager().setCookiesEnabled(true);
             webClient.getOptions().setThrowExceptionOnScriptError(false);
-            HtmlPage page = webClient.getPage("https://jklm.fun/NXSV");
+
+            HtmlPage page = webClient.getPage("https://jklm.fun/QFHK");
             System.out.println(page.getTitleText());
 
+            webClient.waitForBackgroundJavaScript(10_000);
+
+            HtmlInput nicknamePrompt = page.getFirstByXPath("//input[@class='styled nickname']");
+            nicknamePrompt.setValueAttribute("elGoog32");
+            //nicknamePrompt.type("XD323232");
+            //nicknamePrompt.type("\n");
+            HtmlButton submitNameButton = page.getFirstByXPath("//button[.='OK']");
+            submitNameButton.click();
+
+            page.executeJavaScript("loadGame('bombparty')");
+
+            webClient.waitForBackgroundJavaScript(10_000);
+
+            System.out.println(page.getWebResponse().getContentAsString());
+            /*
+            System.out.println(page.getWebResponse().getContentAsString());
+            DomElement gameIframe = page.createElement("iframe");
+            gameIframe.setAttribute("src", "https://falcon.jklm.fun/games/bombparty");
+            HtmlElement gameDiv = page.getFirstByXPath("//div[@class='game']");
+            gameDiv.appendChild(gameIframe);
+
+             */
+
+            /*
+            HtmlButton joinButton = page.getFirstByXPath("//input[@class='styled joinRound']");
+            joinButton.click();
+            webClient.waitForBackgroundJavaScript(5000);
+
+             */
+
+            //HtmlInput nicknameButton = page.getFirstByXPath("//input[@class, 'styled nickname')");
+            //nicknamePrompt.type("XD323232");
+
+            /*
             HtmlInput namePrompt = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/input");
             namePrompt.setValueAttribute("elGoog32");
             HtmlButton submitNameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/button");
@@ -24,18 +62,11 @@ public class main {
             webClient.waitForBackgroundJavaScript(5000);
             System.out.println(page.getWebResponse().getContentAsString());
 
-
-
-
-            HtmlButton joinGameButton = null;
-            while(joinGameButton == null) {
-                System.out.println("Loading...");
-                page = (HtmlPage) webClient.getCurrentWindow().getEnclosedPage();
-                webClient.waitForBackgroundJavaScript(5000);
-                System.out.println(page.getWebResponse().getContentAsString());
-                joinGameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/div[1]/div[1]/button");
-            }
+            HtmlButton joinGameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/div[1]/div[1]/button");
             joinGameButton.click();
+            */
+
+            while (true) {}
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
