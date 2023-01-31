9c53ffb5a56002fbf98806fc198e9adecb4d45e0
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-31 17:33:40 +0100

    Test access to jklm.fun page

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -6,11 +7,10 @@ import java.net.MalformedURLException;
 
 public class main {
     public static void main(String[] args) {
-        try (final WebClient webClient = new WebClient()) {
-            final HtmlPage page = webClient.getPage("https://htmlunit.sourceforge.io/");
+        try (final WebClient webClient = new WebClient(BrowserVersion.FIREFOX)) {
+            webClient.getOptions().setFetchPolyfillEnabled(true);
+            final HtmlPage page = webClient.getPage("https://jklm.fun/PFCF");
             System.out.println(page.getTitleText());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
