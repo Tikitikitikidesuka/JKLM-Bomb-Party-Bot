4df027d79f5161985f8fd27a7f37f25d0ea218b4
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-01-31 19:51:57 +0100

    Test access to jklm.fun page

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,29 @@
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.List;
 
 public class main {
     public static void main(String[] args) {
-        try (final WebClient webClient = new WebClient(BrowserVersion.FIREFOX)) {
+        try (final WebClient webClient = new WebClient()) {
             webClient.getOptions().setFetchPolyfillEnabled(true);
-            final HtmlPage page = webClient.getPage("https://jklm.fun/PFCF");
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setCssEnabled(false);
+            final HtmlPage page = webClient.getPage("https://jklm.fun/GYGE");
             System.out.println(page.getTitleText());
+
+            HtmlInput namePrompt = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/input");
+            namePrompt.setValueAttribute("elGoog");
+            HtmlButton submitNameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/button");
+            submitNameButton.click();
+            webClient.waitForBackgroundJavaScript(50 * 500);
+            System.out.println(page.getWebResponse().getContentAsString());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
