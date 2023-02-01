d9481892c25bec553dca86e16bbffda98a1910be
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-02-01 10:07:12 +0100

    Fix bot not joining room bug

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,74 +1,51 @@
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.*;
-import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLIFrameElement;
-import org.apache.commons.lang3.ObjectUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class main {
     public static void main(String[] args) {
-        try (final WebClient webClient = new WebClient(BrowserVersion.FIREFOX)) {
-            webClient.getOptions().setFetchPolyfillEnabled(true);
-            webClient.getOptions().setJavaScriptEnabled(true);
-            webClient.getOptions().setCssEnabled(true);
-            webClient.getOptions().setRedirectEnabled(true);
-            webClient.getCookieManager().setCookiesEnabled(true);
-            webClient.getOptions().setThrowExceptionOnScriptError(false);
+        /*
+        WebDriver driver = new FirefoxDriver();
 
-            HtmlPage page = webClient.getPage("https://jklm.fun/QFHK");
-            System.out.println(page.getTitleText());
 
-            webClient.waitForBackgroundJavaScript(10_000);
 
-            HtmlInput nicknamePrompt = page.getFirstByXPath("//input[@class='styled nickname']");
-            nicknamePrompt.setValueAttribute("elGoog32");
-            //nicknamePrompt.type("XD323232");
-            //nicknamePrompt.type("\n");
-            HtmlButton submitNameButton = page.getFirstByXPath("//button[.='OK']");
-            submitNameButton.click();
+        WebElement nicknameField = driver.findElement(By.className("styled nickname"));
 
-            page.executeJavaScript("loadGame('bombparty')");
+         */
 
-            webClient.waitForBackgroundJavaScript(10_000);
+        System.setProperty("webdriver.chrome.driver", "/home/miguel/Downloads/chromedriver_linux64(1)/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-            System.out.println(page.getWebResponse().getContentAsString());
-            /*
-            System.out.println(page.getWebResponse().getContentAsString());
-            DomElement gameIframe = page.createElement("iframe");
-            gameIframe.setAttribute("src", "https://falcon.jklm.fun/games/bombparty");
-            HtmlElement gameDiv = page.getFirstByXPath("//div[@class='game']");
-            gameDiv.appendChild(gameIframe);
+        driver.get("https://jklm.fun/HMRR");
 
-             */
+        WebElement nicknameField = driver.findElement(By.className("nickname"));
+        nicknameField.sendKeys("elGoog64");
+        WebElement playButton = driver.findElement(By.xpath("//button[text()='OK']"));
+        playButton.click();
 
-            /*
-            HtmlButton joinButton = page.getFirstByXPath("//input[@class='styled joinRound']");
-            joinButton.click();
-            webClient.waitForBackgroundJavaScript(5000);
-
-             */
-
-            //HtmlInput nicknameButton = page.getFirstByXPath("//input[@class, 'styled nickname')");
-            //nicknamePrompt.type("XD323232");
-
-            /*
-            HtmlInput namePrompt = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/input");
-            namePrompt.setValueAttribute("elGoog32");
-            HtmlButton submitNameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/form/div[2]/button");
-            submitNameButton.click();
-            webClient.waitForBackgroundJavaScript(5000);
-            System.out.println(page.getWebResponse().getContentAsString());
-
-            HtmlButton joinGameButton = page.getFirstByXPath("/html/body/div[2]/div[3]/div[1]/div[1]/button");
-            joinGameButton.click();
-            */
-
-            while (true) {}
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        // Check that game exists
+        if (driver.findElement(By.className("disconnected")).isDisplayed()) {
+            System.out.println("Game not found");
+            driver.quit();
+            System.exit(1);
         }
+
+        System.out.println("Game found");
+
+        driver.switchTo().frame(driver.findElement(By.className("game")).findElement(By.tagName("iframe")));
+        WebElement joinButton = driver.findElement(By.xpath("//button[@data-text='joinGame']"));
+        joinButton.click();
+
+        System.out.println("Game joined");
+
+        while(true) {}
+        //driver.quit();
     }
 }
