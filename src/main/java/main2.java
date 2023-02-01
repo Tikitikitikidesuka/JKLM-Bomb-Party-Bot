2cf9b32faee86b3a57c6080f881e76692533c0c7
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2023-02-01 09:58:41 +0100

    Add working room existence detection

--- src/main/java/main2.java
+++ src/main/java/main2.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class main2 {
@@ -20,28 +21,36 @@ public class main2 {
          */
 
         System.setProperty("webdriver.chrome.driver", "/home/miguel/Downloads/chromedriver_linux64(1)/chromedriver");
-        WebDriver driver = new ChromeDriver(); //Creating an object of FirefoxDriver
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        driver.get("https://jklm.fun/QFHK");
+        driver.get("https://jklm.fun/HMRF");
 
         WebElement nicknameField = driver.findElement(By.className("nickname"));
         nicknameField.sendKeys("elGoog64");
-        WebElement playButton = driver.findElement(By.xpath("/html/body/div[2]/div[3]/form/div[2]/button"));
+        WebElement playButton = driver.findElement(By.xpath("//button[text()='OK']"));
         playButton.click();
 
-        driver.switchTo().frame(driver.findElement(By.xpath("/html/body/div[2]/div[4]/div[1]/iframe")));
+
+        // Check that game exists
+        System.out.println(driver.findElements(By.className("disconnected")));
+        if (driver.findElement(By.className("disconnected")).isDisplayed()) {
+            System.out.println("Game not found");
+            driver.quit();
+            System.exit(1);
+        }
+
+        System.out.println("Game found");
+
+        driver.switchTo().frame(driver.findElement(By.xpath("//button[@data-text='joinGame']")));
         WebElement joinButton = driver.findElement(By.className("joinRound"));
         joinButton.click();
 
-        /*
-        driver.findElement(By.name("q")).sendKeys("Browserstack Guide"); //name locator for text box
-        WebElement searchbutton = driver.findElement(By.name("btnK"));//name locator for google search
-        searchbutton.click();
-         */
+        System.out.println("Game joined");
+
         while(true) {}
         //driver.quit();
     }
