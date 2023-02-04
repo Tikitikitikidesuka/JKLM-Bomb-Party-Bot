e5c378aefafdb06116ee5bba11e4ef31c9df8cbf
GuilledlC <guillermodelacalhacar@gmail.com>
2023-02-04 23:29:53 +0100

    Created and implemented BotConfig

---
+++ src/main/java/BombParty/Bot/BotConfig.java
@@ -0,0 +1,47 @@
+package BombParty.Bot;
+
+import java.nio.file.Path;
+
+public class BotConfig {
+    private final String driver;
+    private final Path driverPath;
+    private final Path dbPath;
+    private long minTypingIntervalMs;
+    private long maxTypingIntervalMs;
+
+    public BotConfig(String driver, Path driverPath, Path dbPath, long typingInterval, long maxTypingIntervalDeviation) {
+        this.driver = driver;
+        this.driverPath = driverPath;
+        this.dbPath = dbPath;
+        this.minTypingIntervalMs = typingInterval;
+        this.maxTypingIntervalMs = maxTypingIntervalDeviation;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public Path getDriverPath() {
+        return driverPath;
+    }
+
+    public Path getDbPath() {
+        return dbPath;
+    }
+
+    public long getMinTypingIntervalMs() {
+        return minTypingIntervalMs;
+    }
+
+    public void setMinTypingIntervalMs(long minTypingIntervalMs) {
+        this.minTypingIntervalMs = minTypingIntervalMs;
+    }
+
+    public long getMaxTypingIntervalMs() {
+        return maxTypingIntervalMs;
+    }
+
+    public void setMaxTypingIntervalMs(long maxTypingIntervalMs) {
+        this.maxTypingIntervalMs = maxTypingIntervalMs;
+    }
+}
