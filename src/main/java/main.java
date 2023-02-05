3c51d27f49292dffb90d261e178f08a15a0afef8
GuilledlC <guillermodelacalhacar@gmail.com>
2023-02-05 01:25:27 +0100

    Main class with correct configs

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,12 +1,28 @@
+import BombParty.Bot.BombPartyBot;
+import BombParty.Bot.BombPartyBotConfig;
 import BombParty.Client.BombPartyClient;
 import BombParty.Client.BombPartyRoom;
 import BombParty.Client.Implementations.Selenium.SeleniumBombPartyClient;
 import BombParty.Client.InvalidWordPlayedException;
 
+import java.nio.file.Paths;
+
 public class main {
-    public static void main(String[] args) {
-        BombPartyClient bpClient = new SeleniumBombPartyClient("webdriver.chrome.driver",
-                "driver/chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
+        BombPartyBotConfig config = new BombPartyBotConfig(
+                "webdriver.chrome.driver",
+                Paths.get("driver/chromedriver.exe"),
+                Paths.get("words.db"),
+                4,
+                12);
+        BombPartyBot bot = new BombPartyBot(config);
+
+        bot.joinRoom("GXVA");
+        while (true) {
+            bot.playRound();
+        }
+        /*BombPartyClient bpClient = new SeleniumBombPartyClient("webdriver.chrome.driver",
+                Paths.get("driver/chromedriver.exe"));
 
         BombPartyRoom room = bpClient.joinRoom("PYVU");
 
@@ -19,6 +35,6 @@ public class main {
 
                 }
             }
-        }
+        }*/
     }
 }
