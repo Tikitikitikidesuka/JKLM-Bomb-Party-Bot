ac10504ba0c35d3ca018b0b36adce8930612df1f
GuilledlC <guillermodelacalhacar@gmail.com>
2023-02-07 00:13:23 +0100

    Properly documented the BombPartyRoom interface

--- src/main/java/BombParty/Client/BombPartyRoom.java
+++ src/main/java/BombParty/Client/BombPartyRoom.java
@@ -1,45 +1,54 @@
 package BombParty.Client;
 
 /**
- * The BombPartyRoom interface provides methods to properly play a BombParty game
+ * The BombPartyRoom interface provides methods to properly play a BombParty game.
  */
 public interface BombPartyRoom {
     /**
+     * Gets the ID of the current room.
+     *
      * @return the ID of the current room
      */
     String getId();
 
     /**
+     * Gets the BombPartyTurnData of the last turn.
+     *
      * @return the BombPartyTurnData of the last turn
      */
     BombPartyTurnData getLastTurnData();
 
     /**
-     * Joins the round if it is possible
+     * Joins the round if it is possible.
      */
     void joinRound();
 
     /**
+     * Gets the {@link BombPartyTurnData}
+     * of the Client's turn, or null if the game has ended.
+     *
      * @return the {@link BombPartyTurnData}
      * of the Client's turn, or null if the game has ended
      */
     BombPartyTurnData waitTurn();
 
     /**
-     * Types the word in the for other players to see but does not submit it
+     * Types the word in the for other players to see but does not submit it.
+     *
      * @param word the word to type
      */
     void typeWord(String word);
 
     /**
-     * Types the word and submit it
+     * Types the word and submits it.
+     *
      * @param word the word to type
      * @throws InvalidWordPlayedException if the word played isn't valid
      */
     void playWord(String word) throws InvalidWordPlayedException;
 
     /**
-     * Leaves the round if it is possible
+     * Leaves the round if it is possible.
      */
     void exit();
 }
